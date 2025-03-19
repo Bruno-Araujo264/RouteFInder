287bb78c45095b890bb05a7a1453d708a9816981
GiovanaZukauskas <giovana.ratao@sptech.school>
2025-03-19 19:11:34 -0300

    Alteração na fonte do sobre nós

--- Datalogs/src/main/java/school/sptech/Main.java
+++ Datalogs/src/main/java/school/sptech/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Functions getDataSystem = new Functions();
 
-        String dataHoraAtual = getDataSystem.getSystemDateTime();
+String dataHoraAtual = getDataSystem.getSystemDateTime();
         String prefixo = "INFO";
-        String mensagem = "Coletando os dados do arquivo XPTO...";
+        String mensagem = "Coletando os dados das Ruas de São Paulo...";
 
         for (int i = 0; i <= 100 ; i++){
             System.out.println(String.format("[%s] [%s] - %s", dataHoraAtual, prefixo, mensagem));
